8e3a01b07ab15dc8ff6d642696ac08c0925c4a03
yuyq18 <yuyq18@mails.tsinghua.edu.cn>
2024-02-10 19:15:42 +0800

    add script for reproducing experiment results

---
+++ visual_results/result_script/Coat.sh
@@ -0,0 +1,24 @@
+# 1. Offline RL(Batch RL) (offpolicy)
+python examples/policy/run_DiscreteBCQ.py --env CoatEnv-v0  --seed 2023 --cuda 7 --epoch 100 --num_leave_compute 7 --leave_threshold 6  --which_tracker avg --reward_handle "cat"  --unlikely-action-threshold 0.6 --window_size 3 --read_message "pointneg" --step-per-epoch 1000 --is_exploration_noise --explore_eps 0.4
+python examples/policy/run_DiscreteCQL.py --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat"  --unlikely-action-threshold 0.6 --window_size 3 --read_message "pointneg"  --step-per-epoch 1000 --is_exploration_noise
+python examples/policy/run_DiscreteCRR.py --env CoatEnv-v0  --seed 2023 --cuda 0 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg" --step-per-epoch 1000 --is_exploration_noise
+python examples/advance/run_SQN.py        --env CoatEnv-v0  --seed 2023 --cuda 1 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg"  --step-per-epoch 1000 --which_head "shead" --is_exploration_noise
+
+# 2. Online RL with User Model (Model-based or simulation-based RL) 
+# 2.1 offpolicy
+python examples/policy/run_DQN.py      --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --is_exploration_noise
+python examples/policy/run_C51.py      --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --is_exploration_noise
+python examples/policy/run_DDPG.py --env CoatEnv-v0 --seed 2023 --cuda 4 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --is_exploration_noise --remap_eps 0.001 --explore_eps 1.5
+python examples/policy/run_TD3.py --env CoatEnv-v0 --seed 2023 --cuda 5 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --policy-noise 0.25 --is_exploration_noise --remap_eps 0.001 --explore_eps 1.3
+
+# 2.2 onpolicy
+python examples/policy/run_PG.py      --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg"  --no_exploration_noise
+python examples/policy/run_A2C.py     --env CoatEnv-v0  --seed 2023 --cuda 7 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --lr 0.005  --ent-coef 0.01 
+python examples/policy/run_PPO.py     --env CoatEnv-v0  --seed 2023 --cuda 6 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg"  --vf-coef 0.25 --eps-clip 0.3 --message "PPOlr0.011" --no_exploration_noise --lr 0.011
+python examples/policy/run_ContinuousPG.py      --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --no_exploration_noise
+python examples/policy/run_ContinuousA2C.py     --env CoatEnv-v0  --seed 2023 --cuda 1 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --no_exploration_noise
+python examples/policy/run_ContinuousPPO.py     --env CoatEnv-v0  --seed 2023 --cuda 2 --epoch 100  --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg" --no_exploration_noise
+
+python examples/advance/run_DORL.py        --env CoatEnv-v0  --seed 2023 --cuda 1 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg" --no_exploration_noise  --lambda_entropy 3 --lr 0.005  --ent-coef 0.01  --no_feature_level
+python examples/advance/run_Intrinsic.py        --env CoatEnv-v0  --seed 2023 --cuda 0 --epoch 100 --num_leave_compute 7 --leave_threshold 6 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg" --no_exploration_noise --lambda_diversity 0.1  --lambda_novelty 0.05 --lr 0.005  --ent-coef 0.01
+
---
+++ visual_results/result_script/MovieLens.sh
@@ -0,0 +1,23 @@
+# 1. Offline RL(Batch RL) (offpolicy)
+python examples/policy/run_DiscreteCRR.py --env MovieLensEnv-v0  --seed 2023 --cuda 3 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg3"  --message "DiscreteCRR"
+python examples/policy/run_DiscreteCQL.py --env MovieLensEnv-v0  --seed 2023 --cuda 5 --epoch 100 --explore_eps 0.5 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat"  --num-quantiles 20 --min-q-weight 10 --window_size 3 --read_message "pointneg3"  --message "DiscreteCQL"
+python examples/policy/run_DiscreteBCQ.py --env MovieLensEnv-v0  --seed 2023 --cuda 3 --epoch 100 --explore_eps 0.5 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat"  --unlikely-action-threshold 0.6 --window_size 3 --read_message "pointneg3"  --message "DiscreteBCQ"
+python examples/advance/run_SQN.py        --env MovieLensEnv-v0  --seed 2023 --cuda 7 --epoch 100 --step-per-epoch 10000 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat"  --window_size 3 --read_message "pointneg3"  --message "SQN"
+
+# 2. Online RL with User Model (Model-based or simulation-based RL) 
+# 2.1 offpolicy
+python examples/policy/run_DQN.py     --env MovieLensEnv-v0  --seed 2023 --cuda 0 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "DQN"
+python examples/policy/run_C51.py     --env MovieLensEnv-v0  --seed 2023 --cuda 2 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "C51"
+python examples/policy/run_DDPG.py    --env MovieLensEnv-v0  --seed 2023 --cuda 2 --epoch 100 --step-per-epoch 30000 --explore_eps 1.0 --remap_eps 0.001 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "DDPG"
+python examples/policy/run_TD3.py     --env MovieLensEnv-v0  --seed 2023 --cuda 3 --epoch 100 --explore_eps 0.9 --remap_eps 0.9 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "TD3"
+
+# 2.2 onpolicy
+python examples/policy/run_A2C.py           --env MovieLensEnv-v0  --seed 2023 --cuda 1 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg_1"  --message "A2C"
+python examples/policy/run_ContinuousA2C.py --env MovieLensEnv-v0  --seed 2023 --cuda 6 --epoch 100 --remap_eps 0 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "ContinuousA2C"
+python examples/policy/run_PG.py            --env MovieLensEnv-v0  --seed 2023 --cuda 2 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "PG"
+python examples/policy/run_ContinuousPG.py  --env MovieLensEnv-v0  --seed 2023 --cuda 0 --epoch 100 --remap_eps 0 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "ContinuousPG"
+python examples/policy/run_PPO.py           --env MovieLensEnv-v0  --seed 2023 --cuda 3 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "DiscretePPO"
+python examples/policy/run_ContinuousPPO.py --env MovieLensEnv-v0  --seed 2023 --cuda 6 --epoch 100 --remap_eps 0 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --window_size 3 --read_message "pointneg3"  --message "ContinuousPPO"
+
+python examples/advance/run_DORL.py       --env MovieLensEnv-v0  --seed 2023 --cuda 1 --epoch 100 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --lambda_entropy 5     --window_size 3 --read_message "pointneg3"  --message "DORL"
+python examples/advance/run_Intrinsic.py  --env MovieLensEnv-v0  --seed 2023 --cuda 6 --epoch 100 --step-per-epoch 50000 --leave_threshold 75 --num_leave_compute 7 --which_tracker avg --reward_handle "cat" --lambda_diversity 0.2 --lambda_novelty 0.2 --window_size 3 --read_message "pointneg3"  --message "Intrinsic"
